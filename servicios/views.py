2d77f9284a7eee3669f7c2846cf855759b58920e
Leghlimi <hlimileg@gmail.com>
2026-05-15 07:17:02 +0200

    Modelo de servicios y  BD OK

--- servicios/views.py
+++ servicios/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from servicios.models import Servicio
 
+''''
+def lista_servicios(request):
+    servicios = Servicio.objects.all()
+    contexto_servicios_servicios = {'lista_servicios': servicios}
+    return render(request, 'servicios/lista_servicios.html', contexto_servicios_servicios)
+'''
+
 def lista_servicios(request):
     servicios = Servicio.objects.all().order_by('id')  # mantenemos un orden consistente
     paginator = Paginator(servicios, 6)  # mostramos 6 servicios por página
@@ -15,8 +22,8 @@ def lista_servicios(request):
 
     # Pasamos a la plantilla como 'lista_servicios'
     contexto_catalogo_servicios = {'lista_servicios': page_obj}
-    return render(request, 'catalogo/lista_servicios.html', contexto_catalogo_servicios)
-
+    return render(request, 'servicios/lista_servicios.html', contexto_catalogo_servicios)
+'''
 def detalle_servicio(request, pk):
     # Obtenemos el servicio concreto o mostramos un 404 si no existe
     servicio = get_object_or_404(Servicio, pk=pk)
@@ -25,4 +32,6 @@ def detalle_servicio(request, pk):
     contexto = {'servicio': servicio}
 
     # Renderizamos la plantilla de detalle
-    return render(request, 'catalogo/detalle_servicio.html', contexto)
+    return render(request, 'servicios/detalle_servicio.html', contexto)
+'''
+
