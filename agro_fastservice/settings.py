ca19042d18951e23debac8fb68229a46969f2d39
Leghlimi <hlimileg@gmail.com>
2026-05-21 21:10:01 +0200

    Fix whitenoise middleware y STATIC_ROOT

--- agro_fastservice/settings.py
+++ agro_fastservice/settings.py
@@ -54,13 +54,14 @@ INSTALLED_APPS = [
 # ---------------------------------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 # ---------------------------------------------------
@@ -154,9 +155,8 @@ USE_TZ = True
 # ---------------------------------------------------
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = '/tmp/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-WHITENOISE_ROOT = BASE_DIR / 'static'
 
 # ---------------------------------------------------
 # Configuración de usuarios
