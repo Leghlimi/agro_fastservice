f93fa59c3edb34944725325d86d693f36707d818
Leghlimi <hlimileg@gmail.com>
2026-05-21 20:42:32 +0200

    Fix whitenoise static files

--- agro_fastservice/settings.py
+++ agro_fastservice/settings.py
@@ -156,6 +156,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_ROOT = BASE_DIR / 'static'
 
 # ---------------------------------------------------
 # Configuración de usuarios
